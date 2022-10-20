9943f98ccf376adefe2514fb59a7fbd05d496f23
inab1822 <inab1822@naver.com>
2022-10-20 11:35:25 +0900

    test

--- js/script.js
+++ js/script.js
@@ -68,5 +68,47 @@ $(document).ready(function(){
         let text = $('.msg').find('.text').val();
    })
 
+
+   var typingBool = false;
+   var typingIdx=0;
+   var liIndex=0;
+   var liLength = $('.typing-text>ul>li').length;
+   //typing-text 안에 있는 ul 안에있는 li 요소의 갯수
+   console.log(liLength);
+   var typingTxt = $('.typing-text>ul>li').eq(liIndex).text()
+   typingTxt = typingTxt.split('');
+   
+   if(typingBool==false){
+    typingBool=true;
+    var tyInt = setInterval(typing,100);
+   }
+
+   function typing(){
+    $('.typing ul li').removeClass('on');
+    $('.typing ul li').eq(liIndex).addClass('on');
+
+    if(typingIdx<typingTxt.length){
+        $('.typing ul li').eq(liIndex).append(typingTxt[typingIdx])
+        typingIdx++;
+    }else if(liIndex<liLength - 1){
+        liIndex++;
+        typingIdx=0;
+        typingBool = false;
+        typingTxt = $('.typing-text>ul>li').eq(liIndex).text();
+        console.log(typingTxt)
+
+        clearInterval(tyInt);
+
+        setTimeout(function(){
+            tyInt = setInterval(typing,100)
+        },1000);
+    }else if(liIndex==liLength -1){
+        clearInterval(tyInt);
+    }
+    
+        
+    }
+   
+
     refresh();
 })
